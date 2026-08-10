67bc5d067e3f21bc19e0ebbc03c2f2cffd6d8c89
Michael L Jackson <michaelljackson101@gmail.com>
2026-08-10 14:49:03 -0400

    Remediate exposed LinkedIn profile source and Wiki content

--- quartz.config.ts
+++ quartz.config.ts
@@ -17,7 +17,7 @@ const config: QuartzConfig = {
     },
     locale: "en-US",
     baseUrl: "quartz.jzhao.xyz",
-    ignorePatterns: ["private", "templates", ".obsidian", "Drafts"],
+    ignorePatterns: ["private", "templates", ".obsidian", "Drafts", "Wiki"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
